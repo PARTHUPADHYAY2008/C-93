7cedc3ffcea4273326183bcf40179067b336bb62
PARTHUPADHYAY2008 <73583558+PARTHUPADHYAY2008@users.noreply.github.com>
2022-01-27 18:31:22 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCDufx7zFcQvYQd47WDvmc9Oh8AeWwA34g",
+  authDomain: "story-telling-app-93e52.firebaseapp.com",
+  projectId: "story-telling-app-93e52",
+  storageBucket: "story-telling-app-93e52.appspot.com",
+  messagingSenderId: "648172266717",
+  appId: "1:648172266717:web:ed3e6891f7a4543974a734"
+};
